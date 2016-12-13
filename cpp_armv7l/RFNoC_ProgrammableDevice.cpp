3fb1d3835a6f8bc99466ca62689eb0eddd317abc
Patrick Wolfram <pwolfram@geontech.com>
2016-12-12 19:00:14 -0500

    Removed check for status.

--- cpp_armv7l/RFNoC_ProgrammableDevice.cpp
+++ cpp_armv7l/RFNoC_ProgrammableDevice.cpp
@@ -169,14 +169,12 @@ void RFNoC_ProgrammableDevice_i::releaseObject() throw (CF::LifeCycle::ReleaseEr
 {
     LOG_TRACE(RFNoC_ProgrammableDevice_i, __PRETTY_FUNCTION__);
 
-    LOG_DEBUG(RFNoC_ProgrammableDevice_i, "Checking device to HW status map for ACTIVE or PENDING statuses");
+    LOG_DEBUG(RFNoC_ProgrammableDevice_i, "Unloading HW statuses");
 
     for (deviceHwStatusMap::iterator it = this->deviceIDToHwStatus.begin(); it != this->deviceIDToHwStatus.end(); ++it) {
         LOG_DEBUG(RFNoC_ProgrammableDevice_i, "Checking status with load filepath: " << it->second.load_filepath);
 
-        if (it->second.state == HW_LOAD::ACTIVE or it->second.state == HW_LOAD::PENDING) {
-            unloadHardware(it->second);
-        }
+        unloadHardware(it->second);
     }
 
     RFNoC_ProgrammableDevice_prog_base_type::releaseObject();
